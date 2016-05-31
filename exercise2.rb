fd78aedeb8b4454bf5a36a215a6a9a9c94159d65
danielvallecl <daniel.vallecl@gmail.com>
2016-05-31 14:06:40 -0400

    Added Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+#Tip Exercise#
+
+tip = 55
+
+puts tip * 0.15
+
+#Exercise 2#
+
+"Hello, my name is Daniel" + " " + 10.to_s
+
+#Exercise 3#
+
+x = 45628
+y = 7839
+
+puts "When adding #{x} and #{y} you get #{x + y} as a result!"
+
+#Exercise 4#
+
+#Exercice 4 equals to true because the right side of is made true by the ! in front.
