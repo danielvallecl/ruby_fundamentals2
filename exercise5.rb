0b72c22e625608e606be31a5f8aec953bb5c665a
danielvallecl <daniel.vallecl@gmail.com>
2016-06-01 14:35:35 -0400

    Adding Exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+#Exercise 5#
+
+puts "What's the temperature in Fahrenheit?"
+temperature = gets.chomp
+
+def conversion(temperature)
+  c = (temperature - 32) * 5/9
+end
+
+puts "The current temperature is #{conversion(temperature.to_i)}"
